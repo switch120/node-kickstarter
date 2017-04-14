32d73007d85d5033aa9e67d5c377f2bd304f6fdd
switch120 <scott.byers@aethercode.com>
2017-04-14 17:53:02 -0400

    - vagrant setup

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const multer = require('multer');
 
 const upload = multer({ dest: path.join(__dirname, 'uploads') });
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 /**
  * Controllers (route handlers).
  */
